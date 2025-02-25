07736a4458bdab3356393926c7601c9ac3358bed
Rebecca Hatch <rhatch88@gmail.com>
2025-02-25 15:15:35 -0500

    connecting to Github

---
+++ Controllers/EventController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TheirDay2.Controllers
+{
+    public class EventController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Event.cs
@@ -0,0 +1,13 @@
+﻿namespace TheirDay2.Models
+{
+    public class Event
+    {
+        public int Id { get; set; } // This is the primary key
+        public string Name { get; set; }
+        public string AgeGroup { get; set; }
+        public decimal Price { get; set; }
+        public string Location { get; set; }
+        public bool IsOutdoor { get; set; }
+
+    }
+}
